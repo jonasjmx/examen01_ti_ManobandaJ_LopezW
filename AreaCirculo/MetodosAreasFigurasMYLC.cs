3eedd012a6d148096fe98e02f07459aec116d745
JONASJMX <jonasjmx@gmail.com>
2025-05-08 10:03:55 -0500

    s2

--- AreaCirculo/MetodosAreasFigurasMYLC.cs
+++ AreaCirculo/MetodosAreasFigurasMYLC.cs
@@ -7,35 +7,35 @@ using System.Threading.Tasks;
 
 namespace AreaCirculo
 {
-    public class Circle_APXX : IAreaCalculator_MYLC
+    public class Circle_MYLC : IAreaCalculator_MYLC
     {
         private double _radius;
-        public Circle_APXX(double radius) => _radius = radius;
+        public Circle_MYLC(double radius) => _radius = radius;
 
         public double CalculateArea_APXX() => Math.PI * _radius * _radius;
     }
 
-    public class Square_APXX : IAreaCalculator_MYLC
+    public class Square_MYLC : IAreaCalculator_MYLC
     {
         private double _side;
-        public Square_APXX(double side) => _side = side;
+        public Square_MYLC(double side) => _side = side;
 
         public double CalculateArea_APXX() => _side * _side;
     }
 
-    public class Sphere_APXX : IAreaCalculator_MYLC, IVolumeCalculator_MYLC
+    public class Sphere_MYLC : IAreaCalculator_MYLC, IVolumeCalculator_MYLC
     {
         private double _radius;
-        public Sphere_APXX(double radius) => _radius = radius;
+        public Sphere_MYLC(double radius) => _radius = radius;
 
         public double CalculateArea_APXX() => 4 * Math.PI * _radius * _radius;
         public double CalculateVolume_APXX() => (4.0 / 3.0) * Math.PI * Math.Pow(_radius, 3);
     }
 
-    public class Cube_APXX : IAreaCalculator_MYLC, IVolumeCalculator_MYLC
+    public class Cube_MYLC : IAreaCalculator_MYLC, IVolumeCalculator_MYLC
     {
         private double _side;
-        public Cube_APXX(double side) => _side = side;
+        public Cube_MYLC(double side) => _side = side;
 
         public double CalculateArea_APXX() => 6 * _side * _side;
         public double CalculateVolume_APXX() => _side * _side * _side;
